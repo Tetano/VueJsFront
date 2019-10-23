e0df8885fbce0959d45118cbeb3c2dbc28450463
Tetano <viniciusvicco@gmail.com>
2019-10-23 13:43:03 -0300

    Initial commit

---
+++ vuejs/main.js
@@ -0,0 +1,25 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        produto: 'Tabajara Motors',
+        imagem : '../assets/unera.jpg'
+    }
+})
+//
+// var tst = new Vue({
+//     el: '#tst',
+//     data: {
+//         message: "Página carregada em: " + new Date().toLocaleString(),
+//         texto: "Eis um pequeno texto aqui".length,
+//         teste : 5,
+//         product: 5
+//     }
+// })
+//
+// var nomes = new Vue({
+//     el: '#nomes',
+//     data: {
+//         nome: 'vinicius',
+//         sobrenome: 'miranda'
+//     }
+// })
